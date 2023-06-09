af73b2b19cbc49466d0d2565a0371ff277992b61
LemonCrow <lemon_crow@naver.com>
2023-06-09 18:51:10 +0900

    최종 클래스 5개로 너프 사과, 당근, 토마토, 감자, 딸기
    정상 작동 확인
    근데 좀 정확도가 떨어져서
    사과, 당근, 토마토 정도만 잘 인식

--- app/src/main/java/kr/co/js/stepscaletoo/CameraPreview.java
+++ app/src/main/java/kr/co/js/stepscaletoo/CameraPreview.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.Pair;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.TextView;
+
 import org.tensorflow.lite.Interpreter;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,7 +27,6 @@ import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
@@ -31,7 +35,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private SurfaceHolder holder;
     private Camera camera;
     private Interpreter interpreter;
-    public Context context;
+    private List<String> classes;
+    private Context context;
+    private TextView resultTextView;
 
     public CameraPreview(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -40,18 +46,8 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         this.context = context;
     }
 
-
-
-    private Pair<Integer, Float> getMaxIndexAndValue(float[] array) {
-        int maxIndex = 0;
-        float maxValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > maxValue) {
-                maxValue = array[i];
-                maxIndex = i;
-            }
-        }
-        return new Pair<>(maxIndex, maxValue);
+    public void setResultTextView(TextView textView) {
+        this.resultTextView = textView;
     }
 
     @Override
@@ -70,97 +66,11 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             public void onPreviewFrame(byte[] data, Camera camera) {
                 // 카메라 프레임 데이터 처리
                 Camera.Size previewSize = camera.getParameters().getPreviewSize();
-                processFrame(data, previewSize.width, previewSize.height);
+                Bitmap bitmap = convertToBitmap(data, previewSize.width, previewSize.height);
+                processImage(bitmap);
             }
-
         });
     }
-
-    private void processFrame(byte[] data, int width, int height) {
-        // TensorFlow 모델에 입력 데이터 전달
-        ByteBuffer inputBuffer = preprocessInput(data, width, height);
-        inputBuffer.order(ByteOrder.nativeOrder());
-
-        int numClasses = 9;
-
-        // 객체 분류 결과를 담을 버퍼
-        float[][] output = new float[1][numClasses];
-
-        // TensorFlow 모델 실행
-        interpreter.run(inputBuffer, output);
-
-        // 결과 처리
-        Pair<Integer, Float> maxIndexAndValue = getMaxIndexAndValue(output[0]);
-        int predictedIndex = maxIndexAndValue.first;
-        float confidence = maxIndexAndValue.second;
-
-        List<String> labels = loadLabels(context, "classes.txt");
-        String predictedClass = labels.get(predictedIndex);
-
-        Log.d("Object Recognition", "Predicted Class: " + predictedClass);
-        Log.d("Object Recognition", "Confidence: " + confidence);
-        Log.d("Object Recognition", "Confidence: " + output[0][0]);
-    }
-
-    private ByteBuffer preprocessInput(byte[] data, int width, int height) {
-        int inputWidth = 224;// 입력 데이터의 가로 크기를 모델과 일치하도록 설정해야 합니다.
-        int inputHeight = 224;// 입력 데이터의 세로 크기를 모델과 일치하도록 설정해야 합니다.
-
-        // 중앙 부분을 중점으로 데이터를 잘라냅니다.
-        int left = (width - inputWidth) / 2;
-        int top = (height - inputHeight) / 2;
-        int right = left + inputWidth;
-        int bottom = top + inputHeight;
-
-        // 잘라낸 데이터를 새로운 배열에 복사합니다.
-        byte[] croppedData = new byte[inputWidth * inputHeight];
-        for (int row = top; row < bottom; row++) {
-            System.arraycopy(data, (row * width + left) * 3, croppedData, (row - top) * inputWidth, inputWidth);
-        }
-
-        ByteBuffer inputBuffer = ByteBuffer.allocateDirect(inputWidth * inputHeight * 3 * 4);
-        inputBuffer.order(ByteOrder.nativeOrder());
-
-        // 잘라낸 데이터를 복사합니다.
-        inputBuffer.put(croppedData);
-
-        return inputBuffer;
-    }
-
-
-    private float normalizePixelValue(byte[] data) {
-        // 입력 데이터를 정규화하는 작업을 수행
-        float pixelValue = (float) (data[0]);
-
-        return pixelValue;
-    }
-
-    private List<String> loadLabels(Context context, String labelsFileName) {
-        List<String> labels = new ArrayList<>();
-        BufferedReader reader = null;
-        try {
-            InputStream inputStream = context.getAssets().open(labelsFileName);
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                labels.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            // 예외 처리 코드 추가
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    // 예외 처리 코드 추가
-                }
-            }
-        }
-        return labels;
-    }
-
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -191,12 +101,15 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             e.printStackTrace();
         }
 
-        // 모델 파일 로드
+        // Load the model file
         try {
             MappedByteBuffer modelFile = loadModelFile(context, "model.tflite");
 
-            // TensorFlow Interpreter 생성
+            // TensorFlow Interpreter creation
             interpreter = new Interpreter(modelFile);
+
+            // Load the classes from classes.txt
+            classes = loadClasses(context, "classes.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -207,7 +120,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         camera.stopPreview();
         camera.release();
 
-        // TensorFlow Interpreter 해제
+        // TensorFlow Interpreter closure
         if (interpreter != null) {
             interpreter.close();
         }
@@ -220,6 +133,84 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         long startOffset = fileDescriptor.getStartOffset();
         long declaredLength = fileDescriptor.getDeclaredLength();
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private List<String> loadClasses(Context context, String fileName) {
+        List<String> classes = new ArrayList<>();
+        try {
+            InputStream inputStream = context.getAssets().open(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                classes.add(line);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
+
+    private Bitmap convertToBitmap(byte[] data, int width, int height) {
+        YuvImage yuvImage = new YuvImage(data, ImageFormat.NV21, width, height, null);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, width, height), 80, stream);
+        byte[] jpegData = stream.toByteArray();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        return BitmapFactory.decodeByteArray(jpegData, 0, jpegData.length, options);
+    }
+
+    private void processImage(Bitmap bitmap) {
+        // Resize the bitmap to match the model input size
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+
+        // Convert the bitmap to a ByteBuffer
+        ByteBuffer inputBuffer = ByteBuffer.allocateDirect(224 * 224 * 3 * 4);
+        inputBuffer.order(ByteOrder.nativeOrder());
+        int[] pixels = new int[224 * 224];
+        resizedBitmap.getPixels(pixels, 0, 224, 0, 0, 224, 224);
+        for (int pixel : pixels) {
+            float r = (float) ((pixel >> 16) & 0xFF);
+            float g = (float) ((pixel >> 8) & 0xFF);
+            float b = (float) (pixel & 0xFF);
+            inputBuffer.putFloat(r);
+            inputBuffer.putFloat(g);
+            inputBuffer.putFloat(b);
+        }
+
+        // Run the inference
+        float[][] output = new float[1][classes.size()];
+        interpreter.run(inputBuffer, output);
+
+        // Find the predicted class and its probability
+        Pair<String, Float> prediction = findMaxClass(output);
+        String className = prediction.first;
+        float probability = prediction.second;
+
+        // Display the result
+        String result = "Class: " + className + "\nProbability: " + probability +
+                "\noutput[0][0]" + output[0][0] +
+                "\noutput[0][1]" + output[0][1] +
+                "\noutput[0][2]" + output[0][2] +
+                "\noutput[0][3]" + output[0][3] +
+                "\noutput[0][4]" + output[0][4];
+
+        resultTextView.setText(result);
+    }
+
+    private Pair<String, Float> findMaxClass(float[][] array) {
+        int maxIndex = 0;
+        float maxValue = array[0][0];
+        for (int col = 1; col < array[0].length; col++) {
+            if (array[0][col] > maxValue) {
+                maxValue = array[0][col];
+                maxIndex = col;
+            }
+        }
+        String className = classes.get(maxIndex);
+        float probability = maxValue;
+        return new Pair<>(className, probability);
     }
 
     private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int width, int height) {
@@ -254,8 +245,5 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         return optimalSize;
     }
-
-
 }
-
 
--- app/src/main/java/kr/co/js/stepscaletoo/MainActivity.java
+++ app/src/main/java/kr/co/js/stepscaletoo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,15 +17,23 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
-    public CameraPreview cameraPreview;
+
+    private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Context context = getApplicationContext();
-        cameraPreview = new CameraPreview(context, null);
+
+        // Find the resultTextView from the layout
+        TextView resultTextView = findViewById(R.id.resultTextView);
+
+        // Find the cameraPreview from the layout
+        cameraPreview = findViewById(R.id.cameraPreview);
+
+        // Set the resultTextView to the cameraPreview
+        cameraPreview.setResultTextView(resultTextView);
     }
 
-
+    // Rest of the MainActivity code
 }
