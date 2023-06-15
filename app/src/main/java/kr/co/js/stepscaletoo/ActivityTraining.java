cb1294f971e40a56025813e12a17d4b738dc8e55
LemonCrow <lemon_crow@naver.com>
2023-06-15 20:11:10 +0900

    2023-06-15 activity java파일 수정 근데 이거 안에 onCreate 없어서 넣어야 할꺼임 아마도?

--- app/src/main/java/kr/co/js/stepscaletoo/ActivityTraining.java
+++ app/src/main/java/kr/co/js/stepscaletoo/ActivityTraining.java
@@ -9,22 +9,5 @@ import android.os.Bundle;
 
 public class ActivityTraining extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_training);
 
-        // SharedFragment 인스턴스 생성
-        SharedFragment sharedFragment = new SharedFragment();
-
-        // 프래그먼트 매니저 가져오기
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        // 프래그먼트 트랜잭션 시작
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        // 프래그먼트를 추가하고 커밋
-        fragmentTransaction.add(R.id.bottom_navigation, sharedFragment);
-        fragmentTransaction.commit();
-    }
 }
